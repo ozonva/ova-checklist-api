d39d14e7ff599e98f5077869646574156511e9df
Dmitry Mamylin <dmamylin@gmail.com>
2021-08-20 02:57:33 +0500

    Add Flusher and Repo interfaces

---
+++ internal/repo/repo.go
@@ -0,0 +1,10 @@
+package repo
+
+import "ova-checklist-api/internal/types"
+
+// Repo is an interface of a storage which stores entities of type types.Checklist
+type Repo interface {
+	AddChecklists(checklists []types.Checklist) error
+	ListChecklists(limit, offset uint64) ([]types.Checklist, error)
+	DescribeChecklist(checklistId uint64) (*types.Checklist, error)
+}
